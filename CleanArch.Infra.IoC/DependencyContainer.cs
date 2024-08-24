1c2b3db56070102509af0bcd77c9dfe991a93a22
mousumi-vp <mousumipriyadarsinis@gmail.com>
2024-08-24 19:53:11 +0530

    confiigure dependency Container in progarm.cs

---
+++ CleanArch.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,22 @@
+﻿using CleanArc.Domain.Interfaces;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.Services;
+using CleanArch.Infra.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<ICourseService, CourseService>();
+            services.AddScoped<ICourseRepository, CouresRepository>();
+        }
+    }
+}
--- CleanArch.Mvc/Program.cs
+++ CleanArch.Mvc/Program.cs
@@ -1,4 +1,5 @@
 using CleanArch.Infra.Data.Context;
+using CleanArch.Infra.IoC;
 using CleanArch.Mvc.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
@@ -18,6 +19,7 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
 builder.Services.AddControllersWithViews();
+DependencyContainer.RegisterServices(builder.Services);
 
 var app = builder.Build();
 
