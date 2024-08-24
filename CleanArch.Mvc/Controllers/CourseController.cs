1f00e748ecc9f3c869a83e138475072fbbbb1aa6
mousumi-vp <mousumipriyadarsinis@gmail.com>
2024-08-24 20:30:22 +0530

    courses view

---
+++ CleanArch.Mvc/Controllers/CourseController.cs
@@ -0,0 +1,20 @@
+﻿using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArch.Mvc.Controllers
+{
+    public class CourseController : Controller
+    {
+        private readonly ICourseService _courseService;
+        public CourseController(ICourseService courseService)
+        {
+                _courseService = courseService;
+        }
+        public IActionResult Index()
+        {
+            CourseViewModel model = _courseService.GetCourses();
+            return View(model);
+        }
+    }
+}
