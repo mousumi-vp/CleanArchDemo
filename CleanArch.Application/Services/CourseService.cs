8f67b27604ecb1517e39951a83744780c511b7eb
mousumi-vp <mousumipriyadarsinis@gmail.com>
2024-08-24 19:18:25 +0530

    implimentation of services and repository

---
+++ CleanArch.Application/Services/CourseService.cs
@@ -0,0 +1,27 @@
+﻿using CleanArc.Domain.Interfaces;
+using CleanArch.Application.Interfaces;
+using CleanArch.Application.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Application.Services
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+        public CourseViewModel GetCourses()
+        {
+            return new CourseViewModel()
+            {
+                Courses = _courseRepository.GetCourses()
+            };
+        }
+    }
+}
---
+++ CleanArch.Infra.Data/Repository/CouresRepository.cs
@@ -0,0 +1,24 @@
+﻿using CleanArc.Domain.Interfaces;
+using CleanArc.Domain.Model;
+using CleanArch.Infra.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArch.Infra.Data.Repository
+{
+    public class CouresRepository : ICourseRepository
+    {
+        private readonly UniversityDbContext _cts;
+        public CouresRepository(UniversityDbContext cts)
+        {
+            _cts = cts;   
+        }
+        public IEnumerable<Course> GetCourses()
+        {
+            return _cts.Courses;
+        }
+    }
+}
