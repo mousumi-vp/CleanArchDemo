2392983ec395c0a79a549ad5af1f0c322872be54
mousumi-vp <mousumipriyadarsinis@gmail.com>
2024-08-26 00:03:43 +0530

    create courses added

--- CleanArch.Application/Services/CourseService.cs
+++ CleanArch.Application/Services/CourseService.cs
@@ -1,6 +1,8 @@
 ﻿using CleanArc.Domain.Interfaces;
 using CleanArch.Application.Interfaces;
 using CleanArch.Application.ViewModel;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,10 +14,24 @@ namespace CleanArch.Application.Services
     public class CourseService : ICourseService
     {
         private readonly ICourseRepository _courseRepository;
-        public CourseService(ICourseRepository courseRepository)
+        private readonly IMediatorHandler _bus;
+        public CourseService(ICourseRepository courseRepository, IMediatorHandler bus)
         {
             _courseRepository = courseRepository;
+            _bus = bus;
         }
+
+        public void Create(CourseViewModel courseViewModel)
+        {
+            var createCourseCommand = new CreateCourseCommand(
+                   courseViewModel.Name,
+                   courseViewModel.Description,
+                   courseViewModel.ImageUrl
+                 );
+
+            _bus.SendCommand(createCourseCommand);
+        }
+
         public CourseViewModel GetCourses()
         {
             return new CourseViewModel()
